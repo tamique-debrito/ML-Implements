b5a147cec599d528efece41a91b4dc9a2c305b94
Tamique de Brito <tamique@mit.edu>
2019-06-13 17:37:58 -0700

    Implemented decision tree train. Seems to be working. Some sample tests in mini_tests.py.

--- Decision Tree/Decision Tree.py
+++ Decision Tree/Decision Tree.py
@@ -6,12 +6,19 @@ class Node:
         self.left = left
         self.right = right
 
-    def left(self):
+    def getLeft(self):
         return self.left
 
-    def right(self):
+    def getRight(self):
         return self.right
-    
+
+    def setLeft(self, left):
+        self.left = left
+
+    def setRight(self, right):
+        self.right = right
+
+
 class Leaf:
     """
     A leaf class for creating a tree.
@@ -19,8 +26,12 @@ class Leaf:
     def __init__(self, value=None):
         self.value = value
 
-    def value(self):
+    def getValue(self):
         return self.value
+
+    def setValue(self, value):
+        self.value = value
+
 
 class DecisionTree:
     """
@@ -28,9 +39,10 @@ class DecisionTree:
     of Boolean feature vectors.
     """
     def __init__(self):
-        pass
+        self.root = None
+        self.numFeatures = None
 
-    def train(self, data):
+    def train(self, data, numFeatures):
         """
         Trains the decision tree on training data.
         
@@ -40,11 +52,62 @@ class DecisionTree:
 
         Returns None.
         """
-        pass
+        self.numFeatures = numFeatures
+        featuresList = list(range(numFeatures)) # A list of all feature indices
+        root = self.trainAux(data, featuresList)
+
+    def trainAux(self, data, remainingFeatures):
+        """
+        Auxiliary function for training the data.
+        Recursively finds best feature for prediction, then trains on remaining features
+    
+
+        data: a list of (point, label) pairs.
+            point: Boolean vector of features.
+            label: Boolean.
+        remainingFeatures: A list containing the indices of the features left to test on.
+
+        Returns a decision tree for remaining features
+        """
+
+        ### Check for trivial cases: labels all agree or no labels left to train on
+        ###
+        
+        test = self.labelsAllSame(data)
+        if test[0] or len(remainingFeatures) == 0:
+            return Leaf(test[1])
+
+
+        ### Find best label to predict on
+        ###
+        
+        scores = [] # Stores pairs (f, s) where f is feature index and s is the score of f
+        for f in remainingFeatures:
+            scores.append((f, self.featureAccuracy(data, f)))
+        pi = lambda x: x[1] # A projection map to help with finding the maximal second coordinate of scores
+
+        maxFeature = max(scores, key = pi)[0] # Get a (there could be multiple) feature with the maximal score
+
+        yes, no = self.splitOnFeature(data, maxFeature)
+
+
+        ### Create and return subtree
+        ###
+        
+        subtree = Node()
+
+        # Makes two (2) copies of remainingFeatures, expect takes out the current feature, one for each recursive call
+        remFeatCopy1 = [f for f in remainingFeatures if f != maxFeature]
+        remFeatCopy2 = [f for f in remainingFeatures if f != maxFeature]
+
+        subtree.setLeft(self.trainAux(no, remFeatCopy1))
+        subtree.setRight(self.trainAux(yes, remFeatCopy2))
+
+        return subtree
 
     def evaluate(self, example):
         """
-        Predicts label of example.
+        Predicts label of data point.
 
         example: Boolean vector of features.
 
@@ -52,25 +115,70 @@ class DecisionTree:
         """
         pass
 
-    def labelsAllSame(self, subset):
+    def labelsAllSame(self, data):
         """
         Determines whether labels of a subset of data all agree.
 
-        subset: the set of data being tested.
+        data: the list of data being tested.
 
-        Returns a pair of Booleans (allSame, label).
-            allSame: True if all labels are the same, false otherwise.
-            label: if all labels are the same, is the value of all labels.
-                        No specification otherwise.
+        Returns a tuple (allSame, label).
+            allSame:        Boolean:    True if all labels are the same, false otherwise.
+            label:          Boolean:    If all labels are the same, is the value (True/False) of all labels.
+                                                If labels not all same, truth value of most common label
         """
-        pass
+        if len(data) == 0:
+            return True, None
+        last = None
+        length = len(data)
+        numTrue = len([d for d in data if d[1] == True])
+        numFalse = length - numTrue
+        if numTrue == length:
+            return True, True, length
+        elif numFalse == length:
+            return True, False, length
+        else:
+            return False, True if numTrue > numFalse else False
 
-    def featureAccuracy(self, subset, feature):
+    def featureAccuracy(self, data, feature):
         """
         Calculates the accuracy, as proportion of examples correctly predicted,
         when using a particular feature to classify a subset of data.
 
-        subset: the set of examples that the feature's predictive accuracy is tested for.
+        data: the list of examples that the feature's predictive accuracy is tested for.
         feature: the index of the particular feature that is being tested.
+
+        Returns a float representing the proportion of examples that are classified correctly by the current feature.
         """
-        pass
+        # Subset of features where given feature is "no"
+        no = [d for d in data if d[0][feature] == False]
+        # Subset of features where given feature is "no"
+        yes = [d for d in data if d[0][feature] == True]
+        lenNo = len(no); lenYes = len(yes)
+        # Number of "True" labels in "no" set and "yes set respectively
+        numPredictedByNo = len([d for d in no if d[1] == True])
+        numPredictedByYes = len([d for d in yes if d[1] == True])
+        
+        return float(max(numPredictedByNo, lenNo - numPredictedByNo) + max(numPredictedByYes, lenYes - numPredictedByYes)) / float(lenNo + lenYes)
+
+    def splitOnFeature(self, data, feature):
+        """
+        Splits the data into two sets based on whether the value of their feature indicated by the parameter "feature" is True or False.
+
+        data: a list of data to split.
+        feature: the particular feature to split on.
+
+        Returns a tuple (yes, no) which are lists of the subsets of data such that the value of "feature" is yes/no respectively.
+        """
+        return [d for d in data if d[0][feature] == True], [d for d in data if d[0][feature] == False]
+
+def drawTree(node, depth):
+    """
+    A quick function to draw trees
+    """
+    if isinstance(node, Leaf):
+        print("   "*depth + str(node.getValue()))
+    else:
+        drawTree(node.getLeft(), depth + 1)
+        print("   "*depth + "/")
+        print("   "*depth + "\\")
+        drawTree(node.getRight(), depth + 1)
---
+++ Decision Tree/mini_test.py
@@ -0,0 +1,15 @@
+
+D = DecisionTree()
+
+n = D.trainAux([[[False, True], True], [[True, True], False]],[0])
+drawTree(n, 0)
+
+
+n = D.trainAux([[[False, True], True], [[True, True], False],
+                [[True, False], True], [[False, False], False]],[0,1])
+drawTree(n, 0)
+
+
+n = D.trainAux([[[False, True], True], [[True, True], False],
+                [[True, False], True], [[False, False], False]],[0])
+drawTree(n, 0)
